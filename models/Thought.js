6de2b64b8d55894b5d1cb082a4a2e1cd4035bbd2
roxyvaught <roxanna.vaught@gmail.com>
2020-07-26 17:18:41 -0500

    models for index and thoughts

--- models/Thought.js
+++ models/Thought.js
@@ -47,13 +47,13 @@ const ThoughtSchema = new Schema({
 
   }
   );
-    // get total count of reactions on retrieval
+    
     ThoughtSchema.virtual('reactionCount').get(function() {
         return this.reactions.length;
       });
 
-  // create the Thought model using the UserSchema
+ 
 const Thought = model('Thought', ThoughtSchema);
 
-// export the Thought model
+
 module.exports = Thought;
